9d073d93598fd35b8d36aa070e6aa9400fcd0105
Archit695 <100460844+Archit695@users.noreply.github.com>
2022-08-27 18:21:44 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+&#39;hello&#39;
+-87.8
+-
+/
++
+6
+S1)~ 
+Values :-   &#39;hello&#39;
+            -87.8
+            6
+expressions :-  * 
+                -
+                /
+                +2. What is the difference between string and variable?
+S2)~
+String is a type of data or information stored in a variable.String is written in quotes like "Bingo".3. Describe three different data types.
+S3)~
+~Int - They are integers. Integers can be positive, negative and zero.
+~Float - They are floating point number. eg- 97.0 , 58.65
+~complex - They can have two parts, one positive and one negative.4. What is an expression made up of? What do all expressions do?
+S4)~
+An expression is a combination of values, variables, operators, and calls to functions.Any value is an expression.
+An expression is a type of instruction that combines various values and needed to be evaluate to get a result.
+5). This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+S5)~
+An expression is a combination of values, variables, operators, and calls to functions.
+The statement is a command which is to be interpreted by python, eg, input, print. 
+
+6). After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+S6)~
+
+# In[3]:
+
+
+bacon = 23
+bacon+1
+print(bacon)
+
+7). What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam'*3
+S7)~They will be same
+
+# In[6]:
+
+
+'spam'+'spamspam'
+
+
+# In[7]:
+
+
+'spam'*3
+
+8). Why is eggs a valid variable name while 100 is invalid?
+S8)~
+100 is an integer and integer cannot be used as a variable.9). What three functions can be used to get the integer, floating-point number, or string
+version of a value?
+S9)~
+For integer - int()
+For floating - float()
+For string - str()
+10). Why does this expression cause an error? How can you fix it?
+'I have eaten'  + 99 + 'burritos'
+S10)~
+# In[9]:
+
+
+'I have eaten'  + 99 + 'burritos'
+# 99 is an integer and cannot concatenate with string.
+
+
+# In[ ]:
+
+
+
+
